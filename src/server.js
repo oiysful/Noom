3936f3b75928a8c725e287f29c3bc490baa4448a
이용상 <gaonoiys@gmail.com>
2023-02-01 16:19:27 +0900

    2023.02.01.

--- src/server.js
+++ src/server.js
@@ -15,10 +15,15 @@ const handleListen = () => console.log(`Listening on "http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-    console.log(socket);
-}
+const sockets = [];
 
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    console.log("Connected to Browser ✅");
+    socket.on("close", () => console.log("Disconnected from the Browser ❎"));
+    socket.on("message", message => {
+        sockets.forEach(aSocket => aSocket.send(message.toString()));
+    });
+});
 
 server.listen(3000, handleListen);
